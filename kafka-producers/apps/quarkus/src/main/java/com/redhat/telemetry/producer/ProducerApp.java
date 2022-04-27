5e81626582598d6c880683e8e26a19a826038ed8
Thomas Jungbauer <25078868+tjungbauer@users.noreply.github.com>
2022-04-27 13:33:39 +0200

    Update ProducerApp.java

--- kafka-producers/apps/quarkus/src/main/java/com/redhat/telemetry/producer/ProducerApp.java
+++ kafka-producers/apps/quarkus/src/main/java/com/redhat/telemetry/producer/ProducerApp.java
@@ -18,5 +18,20 @@ public class ProducerApp {
     private final Random random = new Random();
 
     // TODO: Implement the Kafka producer
-
+    @Outgoing("kafka-mm2-test-migration-cookbook")
+    public Multi<Record<String, Integer>> generate() {
+            return Multi.createFrom().ticks().every(Duration.ofSeconds(1))
+                    .onOverflow().drop()
+                    .map(tick -> {
+                        String currentDevice = "message-stream-" + random.nextInt(10);
+                        int currentMeasure = random.nextInt(100);
+        
+                        LOG.infov("Test-Message ID: {0}, number: {1}",
+                                currentDevice,
+                                currentMeasure
+                        );
+        
+                        return Record.of(currentDevice, currentMeasure);
+                    });
+    }
 }
